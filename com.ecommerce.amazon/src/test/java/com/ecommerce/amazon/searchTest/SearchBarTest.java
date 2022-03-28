3ea727f507e7cf7da06fca22fb5dbf2e4a613bcf
vineeth konge <vineeth konge@DESKTOP-MQQN6CU>
2022-03-28 13:28:37 +0530

    amazon commit

---
+++ com.ecommerce.amazon/src/test/java/com/ecommerce/amazon/searchTest/SearchBarTest.java
@@ -0,0 +1,15 @@
+package com.ecommerce.amazon.searchTest;
+
+import org.testng.annotations.Test;
+
+public class SearchBarTest {
+	
+	@Test
+	public void enterValuesTest(){
+		System.out.println("entered values successfully");
+	}
+	@Test
+	public void groceryTest() {
+		System.out.println("entered grocery values");
+	}
+}
